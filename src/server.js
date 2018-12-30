a5d76ba76163ca5193442dba8d5ecdd614421b42
Rafaelnascimento <rkrafael.nascimento@gmail.com>
2018-12-29 23:19:02 -0200

    Style Guide eslint/prettier/editorconfig

--- src/server.js
+++ src/server.js
@@ -2,15 +2,14 @@ const fastify = require('fastify')({ logger: true })
 const port = 3000
 
 const start = async () => {
-
-    try {
-        await fastify.listen(port, '0.0.0.0')
-        fastify.log.info(`server listening on ${fastify.server.address().port}`)
-        console.log(`server listening on ${fastify.server.address().port}`)
-    } catch (err) {
-        fastify.log.error(err)
-        process.exit(1)
-    }
+  try {
+    await fastify.listen(port, '0.0.0.0')
+    fastify.log.info(`server listening on ${fastify.server.address().port}`)
+    console.log(`server listening on ${fastify.server.address().port}`)
+  } catch (err) {
+    fastify.log.error(err)
+    process.exit(1)
+  }
 }
 
 start()
