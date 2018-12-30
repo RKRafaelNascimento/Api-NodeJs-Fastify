840106890362d1601fa539d02e51a88f91e19d17
Rafaelnascimento <rkrafael.nascimento@gmail.com>
2018-12-29 22:07:15 -0200

    server.js fastify

---
+++ src/server.js
@@ -0,0 +1,16 @@
+const fastify = require('fastify')({ logger: true })
+const port = 3000
+
+const start = async () => {
+
+    try {
+        await fastify.listen(port, '0.0.0.0')
+        fastify.log.info(`server listening on ${fastify.server.address().port}`)
+        console.log(`server listening on ${fastify.server.address().port}`)
+    } catch (err) {
+        fastify.log.error(err)
+        process.exit(1)
+    }
+}
+
+start()
